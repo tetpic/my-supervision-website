24705e30f32a6642905344954149f42da8ae3d85
tetpic <bull1604@yandex.ru>
2022-02-19 22:53:02 +0300

    main page not adaptive&modals

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,32 +1,48 @@
 "use strict"
 // import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
+
+// ===========слайдер для проектов ============
+
 const projects = new Swiper('.done-projects-swiper', {
-    
     slidesPerView: 'auto',
     spaceBetween: 5,
-    mousewheel: true,
     freeMode: true,
-  
-    
-    
   });
-// import Swiper from 'swiper/bundle';
-// console.log('file 1');
-const swiper = document.querySelector('.done-projects-swiper').swiper;
-
-// console.log(swiper)
+const projectsSwiper = document.querySelector('.done-projects-swiper').swiper;
 
 let projectsLeftBtn = document.querySelector('.swiper-left')
 let projectsRightBtn = document.querySelector('.swiper-right')
 
 projectsLeftBtn.addEventListener('click', function() {
     console.log('pushed-left')
-    swiper.slidePrev();
+    projectsSwiper.slidePrev();
 })
 
 projectsRightBtn.addEventListener('click', function() {
 
     console.log('pushed-right')
-    swiper.slideNext();
+    projectsSwiper.slideNext();
+})
+
+
+// ===============слайдер для менеджеров качества============
+
+const qualityManagers = new Swiper('.quality-swiper', {
+    slidesPerView: 1,
+})
+
+const managersSwiper = document.querySelector('.quality-swiper').swiper;
+
+let managersButtons = document.querySelector('.quality-buttons')
+
+let managerLftBtn = managersButtons.querySelector('.prev-button')
+let managerRightBtn = managersButtons.querySelector('.next-button')
+
+managerLftBtn.addEventListener('click', function() {
+    managersSwiper.slidePrev()
+})
+
+managerRightBtn.addEventListener('click', function() {
+    managersSwiper.slideNext()
 })
